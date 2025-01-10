456037abeca465235ca18725d3275f346ecf8594
MNatan23 <163414251+MNatan23@users.noreply.github.com>
2025-01-10 08:24:12 +0100

    Add project files.

---
+++ nepesseg/Orszag.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg
+{
+    internal class Orszag
+    {
+      
+        
+            public string Orszagnev { get; private set; }
+            public int Terulet { get; private set; }
+            public string Nepesseg { get; private set; }
+            public string Fovaros { get; private set; }
+            public int FovarosNepesseg { get; private set; }
+
+            public Orszag(string orszagnev, int terulet, int nepesseg, string fovaros, int fovarosNepesseg)
+            {
+                Orszagnev = orszagnev;
+                Terulet = terulet;
+                Nepesseg = nepesseg;
+                Fovaros = fovaros;
+                FovarosNepesseg = fovarosNepesseg;
+            }
+
+            public Orszag(string sor)
+             
+            {
+            string[] s = sor.Split(';');
+            Orszagnev = s[0];
+            Terulet =int.Parse( s[1]);
+            Nepesseg = s[2];
+            Fovaros = s[3];
+            FovarosNepesseg =int.Parse (s[4]);
+
+                // Ebben a konstruktorban is elvégezheti az adatsor felbontását!
+                // Ha nem ezt választja, akkor törölje ezt a konstruktort!
+            }
+
+            public override string ToString()
+            {
+                return "";
+            }
+        }
+    }
+
---
+++ nepesseg/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+
+namespace nepesseg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Orszag> orszagok = new();
+            foreach (var sor in File.ReadAllLines("adatok-utf8.txt").Skip(1))
+
+                foreach (var g in orszagok);
+                {
+                if (true)
+                {
+
+                }
+                }
+            {
+
+                orszagok.Add(new Orszag(sor));
+            }
+            Console.WriteLine($"4.feladat \n A beolvasott országainak száma {orszagok.Count}");
+        }
+        
+    }
+}
